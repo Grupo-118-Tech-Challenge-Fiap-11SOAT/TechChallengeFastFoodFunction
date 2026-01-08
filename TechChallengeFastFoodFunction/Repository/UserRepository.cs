58e6c3fb93363c308d224f2d1e6c58f18a93029a
tuliorezende <tulio.rezende@outlook.com>
2026-01-08 09:47:12 -0300

    chore: remove unnecessary try-catch blocks in UserRepository for cleaner code

--- TechChallengeFastFoodFunction/Repository/UserRepository.cs
+++ TechChallengeFastFoodFunction/Repository/UserRepository.cs
@@ -29,27 +29,20 @@ public class UserRepository : IUserRepository
             {
                 command.Parameters.AddWithValue("@Email", username);
 
-                try
+                await connection.OpenAsync();
+                using (var reader = await command.ExecuteReaderAsync())
                 {
-                    await connection.OpenAsync();
-                    using (var reader = await command.ExecuteReaderAsync())
+                    if (await reader.ReadAsync())
                     {
-                        if (await reader.ReadAsync())
+                        // Se encontrar o usuário, retorna o objeto User.
+                        return new Employee
                         {
-                            // Se encontrar o usuário, retorna o objeto User.
-                            return new Employee
-                            {
-                                Name = reader["Name"].ToString(),
-                                Password = reader["Password"].ToString(),
-                                Role = reader["Role"].ToString(),
-                                Email = reader["Email"].ToString()
-                            };
-                        }
+                            Name = reader["Name"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            Role = reader["Role"].ToString(),
+                            Email = reader["Email"].ToString()
+                        };
                     }
-                }
-                catch (SqlException)
-                {
-                    throw;
                 }
             }
         }
@@ -72,25 +65,18 @@ public class UserRepository : IUserRepository
             {
                 command.Parameters.AddWithValue("@Cpf", cpf);
 
-                try
+                await connection.OpenAsync();
+                using (var reader = await command.ExecuteReaderAsync())
                 {
-                    await connection.OpenAsync();
-                    using (var reader = await command.ExecuteReaderAsync())
+                    if (await reader.ReadAsync())
                     {
-                        if (await reader.ReadAsync())
+                        return new Employee
                         {
-                            return new Employee
-                            {
-                                Name = reader["Name"].ToString(),
-                                Cpf = reader["Cpf"].ToString(),
-                                Role = "Customer"
-                            };
-                        }
+                            Name = reader["Name"].ToString(),
+                            Cpf = reader["Cpf"].ToString(),
+                            Role = "Customer"
+                        };
                     }
-                }
-                catch (SqlException)
-                {
-                    throw;
                 }
             }
         }
@@ -104,36 +90,22 @@ public class UserRepository : IUserRepository
             return null;
         }
 
-        try
+        using (var connection = new SqlConnection(_connectionString))
         {
-            using (var connection = new SqlConnection(_connectionString))
+            var sql = "INSERT INTO Employees (Name, Surname, Email, Password, Role, Cpf, BirthDay) OUTPUT INSERTED.Id VALUES (@Name, @Surname, @Email, @Password, @Role, @Cpf, @BirthDay)";
+            using (var command = new SqlCommand(sql, connection))
             {
-                var sql = "INSERT INTO Employees (Name, Surname, Email, Password, Role, Cpf, BirthDay) OUTPUT INSERTED.Id VALUES (@Name, @Surname, @Email, @Password, @Role, @Cpf, @BirthDay)";
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@Name", employee.Name);
-                    command.Parameters.AddWithValue("@Surname", employee.Surname);
-                    command.Parameters.AddWithValue("@Email", employee.Email);
-                    command.Parameters.AddWithValue("@Password", employee.Password);
-                    command.Parameters.AddWithValue("@Role", employee.Role);
-                    command.Parameters.AddWithValue("@Cpf", employee.Cpf);
-                    command.Parameters.AddWithValue("@BirthDay", employee.BirthDay);
-                    try
-                    {
-                        await connection.OpenAsync();
-                        employee.Id = (int)await command.ExecuteScalarAsync();
-                        return employee;
-                    }
-                    catch (SqlException)
-                    {
-                        throw;
-                    }
-                }
+                command.Parameters.AddWithValue("@Name", employee.Name);
+                command.Parameters.AddWithValue("@Surname", employee.Surname);
+                command.Parameters.AddWithValue("@Email", employee.Email);
+                command.Parameters.AddWithValue("@Password", employee.Password);
+                command.Parameters.AddWithValue("@Role", employee.Role);
+                command.Parameters.AddWithValue("@Cpf", employee.Cpf);
+                command.Parameters.AddWithValue("@BirthDay", employee.BirthDay);
+                await connection.OpenAsync();
+                employee.Id = (int)await command.ExecuteScalarAsync();
+                return employee;
             }
-        }
-        catch (Exception)
-        {
-            throw;
         }
     }
 
@@ -144,34 +116,20 @@ public class UserRepository : IUserRepository
             return null;
         }
 
-        try
+        using (var connection = new SqlConnection(_connectionString))
         {
-            using (var connection = new SqlConnection(_connectionString))
+            var sql = "INSERT INTO Customers (Name, Surname,email, Cpf, BirthDay) OUTPUT INSERTED.Id VALUES (@Name, @Surname, @Email, @Cpf, @BirthDay)";
+            using (var command = new SqlCommand(sql, connection))
             {
-                var sql = "INSERT INTO Customers (Name, Surname,email, Cpf, BirthDay) OUTPUT INSERTED.Id VALUES (@Name, @Surname, @Email, @Cpf, @BirthDay)";
-                using (var command = new SqlCommand(sql, connection))
-                {
-                    command.Parameters.AddWithValue("@Name", customer.Name);
-                    command.Parameters.AddWithValue("@Surname", customer.Surname);
-                    command.Parameters.AddWithValue("@Email", customer.Email);
-                    command.Parameters.AddWithValue("@Cpf", customer.Cpf);
-                    command.Parameters.AddWithValue("@BirthDay", customer.BirthDay);
-                    try
-                    {
-                        await connection.OpenAsync();
-                        customer.Id = (int)await command.ExecuteScalarAsync();
-                        return customer;
-                    }
-                    catch (SqlException)
-                    {
-                        throw;
-                    }
-                }
+                command.Parameters.AddWithValue("@Name", customer.Name);
+                command.Parameters.AddWithValue("@Surname", customer.Surname);
+                command.Parameters.AddWithValue("@Email", customer.Email);
+                command.Parameters.AddWithValue("@Cpf", customer.Cpf);
+                command.Parameters.AddWithValue("@BirthDay", customer.BirthDay);
+                await connection.OpenAsync();
+                customer.Id = (int)await command.ExecuteScalarAsync();
+                return customer;
             }
-        }
-        catch (Exception)
-        {
-            throw;
         }
     }
 }
